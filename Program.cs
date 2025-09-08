c3964b7eb40a8eb125134d53d302576420ef64ba
Marcelo Quadros <marcelo.quadros@mercadolivre.com>
2025-09-08 12:41:45 -0300

    remove swagger deps

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
